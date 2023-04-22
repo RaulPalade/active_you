84ddffdef7363f66732c54348cf3c11200d82fe8
Raul Palade <raulpalade@outlook.com>
2023-04-22 17:54:35 +0200

    Added Welcome page

--- lib/pages/intro/welcome_page.dart
+++ lib/pages/intro/welcome_page.dart
@@ -1,28 +1,77 @@
-import 'package:active_you/widgets/buttons/primary_button.dart';
+import 'package:active_you/theme/active_you_theme.dart';
 import 'package:flutter/material.dart';
-
-import '../../widgets/buttons/login_button.dart';
-import '../../widgets/buttons/next_button.dart';
-import '../../widgets/buttons/secondary_button.dart';
-import '../../widgets/buttons/white_button.dart';
+import 'package:active_you/widgets/buttons/white_button.dart';
 
 class WelcomePage extends StatelessWidget {
   const WelcomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text("WELCOME PAGE")),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: const [Center(child: Text("WELCOME"))],
-      ),
-      bottomNavigationBar: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 40.0),
-        child: WhiteButton(
-          title: "Get Started",
-          onClick: () => {},
+    return Container(
+      decoration: const BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [
+            ActiveYouTheme.brandLightColor,
+            ActiveYouTheme.brandDarkColor
+          ])),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image.asset(
+              "assets/images/logo/logo-man.png",
+              width: 120,
+              alignment: Alignment.center,
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: const [
+                Text(
+                  "Active",
+                  style: TextStyle(
+                    fontSize: 36,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: "Poppins-Bold"
+                  ),
+                ),
+                Text(
+                  "You",
+                  style: TextStyle(
+                    fontSize: 36,
+                    fontFamily: "Poppins-Bold",
+                    color: ActiveYouTheme.whiteColor,
+                  ),
+                )
+              ],
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            const Text(
+              "Everybody Can Train",
+              style: TextStyle(
+                fontSize: 18,
+                color: ActiveYouTheme.textGreyColor,
+              ),
+            )
+          ],
+        ),
+        bottomNavigationBar: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: WhiteButton(
+              title: "Get Started",
+              onClick: () => {},
+            ),
+          ),
         ),
       ),
     );
